1203192488a1c74e223e824dc4614cc4cffd1e71
steveminutillo <steveminutillo@a5d648f4-9e1a-0410-98de-79dcf2039bee>
2007-06-03 19:43:08 +0000

    repair a few cases of broken HTML from simplepie
    git-svn-id: http://feed-on-feeds.googlecode.com/svn/trunk@20 a5d648f4-9e1a-0410-98de-79dcf2039bee

--- plugins/fixdivs.php
+++ plugins/fixdivs.php
@@ -4,7 +4,7 @@ fof_add_item_filter('fof_fixdivs');
 
 function fof_fixdivs($text)
 {
-   $text = str_replace('<div"', '<div "', $text);
+   $text = str_ireplace('<div"', '<div "', $text);
    return $text;
 }
 ?>
