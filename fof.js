0752cc051f089af29f13817bedd7ceddf301aa27
steveminutillo <steveminutillo@a5d648f4-9e1a-0410-98de-79dcf2039bee>
2007-06-08 04:21:15 +0000

    clean up indentation and comments
    git-svn-id: http://feed-on-feeds.googlecode.com/svn/trunk@48 a5d648f4-9e1a-0410-98de-79dcf2039bee

--- fof.js
+++ fof.js
@@ -1,6 +1,6 @@
-var curWidth=0;
-var curPos=0;
-var drag=false;
+var curWidth = 0;
+var curPos = 0;
+var drag = false;
 var what;
 var when;
 
@@ -10,309 +10,302 @@ var itemElement;
 
 function keyboard(e)
 {
-	if (!e) e = window.event;
-
-        if (e.keyCode) keycode=e.keyCode;
-        else keycode=e.which;
-        
+    if (!e) e = window.event;
+    
+    if (e.keyCode) keycode=e.keyCode;
+    else keycode=e.which;
+    
     if(e.shiftKey || e.ctrlKey || e.altKey || e.metaKey) return true;
-
-	key = String.fromCharCode(keycode);	
-
-	if(key == "n")
-	{
-		if(itemElement)
-		{
-			Element.removeClassName(itemElement, 'selected');
-
-			next = itemElement.nextSibling;
-			
-			if(next.id)
-			{
-				itemElement = next;
-			}
-			else
-			{
-				item = firstItem;
-				itemElement = $(item);
-			}
-			
-			item = itemElement.id;
-			itemElement = $(item);
-
-			Element.addClassName(itemElement, 'selected');
-
-        	y = itemElement.y ? itemElement.y : itemElement.offsetTop;
-    		window.scrollTo(0, y);
-
-			return false;
-		}
-		else
-		{
-			item = firstItem;
-			itemElement = $(item);
-			Element.addClassName(itemElement, 'selected');
-
-        	y = itemElement.y ? itemElement.y : itemElement.offsetTop;
-    		window.scrollTo(0, y);
-			
-			return false;
-		}
-	}
-	
-	return true;
+    
+    key = String.fromCharCode(keycode); 
+    
+    if(key == "n")
+    {
+        if(itemElement)
+        {
+            Element.removeClassName(itemElement, 'selected');
+            
+            next = itemElement.nextSibling;
+            
+            if(next.id)
+            {
+                itemElement = next;
+            }
+            else
+            {
+                item = firstItem;
+                itemElement = $(item);
+            }
+            
+            item = itemElement.id;
+            itemElement = $(item);
+            
+            Element.addClassName(itemElement, 'selected');
+            
+            y = itemElement.y ? itemElement.y : itemElement.offsetTop;
+            window.scrollTo(0, y);
+            
+            return false;
+        }
+        else
+        {
+            item = firstItem;
+            itemElement = $(item);
+            Element.addClassName(itemElement, 'selected');
+            
+            y = itemElement.y ? itemElement.y : itemElement.offsetTop;
+            window.scrollTo(0, y);
+            
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 
 
 function startResize(e)
 {
-	if (!e) e = window.event;
-
-	Event.stop(e);
-
-	drag = true;
-	curPos=e.clientX;
-	curWidth=$('sidebar').offsetWidth;
-
-	//window.status="startResize: " + curPos + " " + curWidth;
+    if (!e) e = window.event;
+    
+    Event.stop(e);
+    
+    drag = true;
+    curPos=e.clientX;
+    curWidth=$('sidebar').offsetWidth;
 
     return false;
 }
 
 function dragResize(e)
 {
-	if (!e) e = window.event;
-	//window.status="dragResize";
-	
-	if(drag)
-	{
-		Event.stop(e);
+    if (!e) e = window.event;
+    
+    if(drag)
+    {
+        Event.stop(e);
+        
+        newPos=e.clientX;
+        var x=newPos-curPos;
+        var w=curWidth+x;
+        newWidth=(w<5?5:w);
+        
+        $('handle').style.left=newWidth+'px';
 
-		newPos=e.clientX;
-		var x=newPos-curPos;
-		var w=curWidth+x;
-		newWidth=(w<5?5:w);
-
-		//$('sidebar').style.width=newWidth+'px';
-		$('handle').style.left=newWidth+'px';
-		//$('items').style.marginLeft=(newWidth+20)+'px';
-
-		//window.status="startResize: " + curPos + " " + curWidth + " " + newPos + " " + newWidth;
-
-	    return false;
+        return false;
     }
 }
 
 function completeDrag(e)
 {
-	if (!e) e = window.event;
-	//window.status="completeDrag";
+    if (!e) e = window.event;
 
-	if(drag)
-	{
-		Event.stop(e);
-
-		drag = false;
-
-		newPos=e.clientX;
-		var x=newPos-curPos;
-		var w=curWidth+x;
-		newWidth=(w<5?5:w);
-
-		$('sidebar').style.width=newWidth+'px';
-		$('handle').style.left=newWidth+'px';
-		$('items').style.marginLeft=(newWidth+20)+'px';
-		if(isIE)
-		{
-			tables = $$('#sidebar table');
-			for(i=0;i<tables.length;i++){
-				tables[i].style.width=(newWidth-20)+'px';
-			}
-		}		
-		var today = new Date();
-		var expire = new Date();
-		expire.setTime(today.getTime() + 3600000*24*100);
-		document.cookie = "fof_sidebar_width="+newWidth
-			+ "; expires="+expire.toGMTString()+"; path=/";
-
-	    return false;
+    if(drag)
+    {
+        Event.stop(e);
+        
+        drag = false;
+        
+        newPos=e.clientX;
+        var x=newPos-curPos;
+        var w=curWidth+x;
+        newWidth=(w<5?5:w);
+        
+        $('sidebar').style.width=newWidth+'px';
+        $('handle').style.left=newWidth+'px';
+        $('items').style.marginLeft=(newWidth+20)+'px';
+        if(isIE)
+        {
+            tables = $$('#sidebar table');
+            for(i=0;i<tables.length;i++){
+                tables[i].style.width=(newWidth-20)+'px';
+            }
+        }       
+        var today = new Date();
+        var expire = new Date();
+        expire.setTime(today.getTime() + 3600000*24*100);
+        document.cookie = "fof_sidebar_width="+newWidth
+        + "; expires="+expire.toGMTString()+"; path=/";
+        
+        return false;
     }
-
+    
 }
 
 function hide_all()
 {
-	items = document.getElementsByClassName("item", "items");
-	items.each( function(e) { e.className = "item hidden"; });
+    items = document.getElementsByClassName("item", "items");
+    items.each( function(e) { e.className = "item hidden"; });
 }
 
 function show_all()
 {
-	items = document.getElementsByClassName("item", "items");
-	items.each( function(e) { e.className = "item shown"; });
+    items = document.getElementsByClassName("item", "items");
+    items.each( function(e) { e.className = "item shown"; });
 }
 
 function hide_body(id)
 {
-	$('i' + id).className = 'item hidden';
+    $('i' + id).className = 'item hidden';
 }
 
 function show_body(id)
 {
-	$('i' + id).className = 'item shown';
+    $('i' + id).className = 'item shown';
 }
 
 function flag_upto(id)
 {
-	elements = $A(Form.getInputs('itemform', 'checkbox'));
-	
-	for(i=0; i<elements.length; i++)
-	{
-		elements[i].checked = true;
-		
-		if(elements[i].name == id)
-		{
-			break;
-		}
-	}
+    elements = $A(Form.getInputs('itemform', 'checkbox'));
+    
+    for(i=0; i<elements.length; i++)
+    {
+        elements[i].checked = true;
+        
+        if(elements[i].name == id)
+        {
+            break;
+        }
+    }
 }
 
 function toggle_highlight()
 {
-	if(document.body.className == '')
-	{
-		document.body.className = 'highlight-on';
-	}
-	else
-	{
-		document.body.className = '';
-	}
+    if(document.body.className == '')
+    {
+        document.body.className = 'highlight-on';
+    }
+    else
+    {
+        document.body.className = '';
+    }
 }
 
 function flag_all()
 {
-	elements = $A(Form.getInputs('itemform', 'checkbox'));	
-	elements.each( function(e) { e.checked = true; });
+    elements = $A(Form.getInputs('itemform', 'checkbox'));  
+    elements.each( function(e) { e.checked = true; });
 }
 
 
 function toggle_all()
 {
-	elements = $A(Form.getInputs('itemform', 'checkbox'));
-	elements.each( function(e) { e.checked = !e.checked; });
+    elements = $A(Form.getInputs('itemform', 'checkbox'));
+    elements.each( function(e) { e.checked = !e.checked; });
 }
 
 function unflag_all()
 {
-	elements = $A(Form.getInputs('itemform', 'checkbox'));
-	elements.each( function(e) { e.checked = false; });
+    elements = $A(Form.getInputs('itemform', 'checkbox'));
+    elements.each( function(e) { e.checked = false; });
 }
 
 
 function mark_read()
 {
-	document.items['action'].value = 'read';
-	document.items['return'].value = escape(location);
-	document.items.submit();
+    document.items['action'].value = 'read';
+    document.items['return'].value = escape(location);
+    document.items.submit();
 }
 
 function mark_unread()
 {
-	document.items['action'].value = 'unread';
-	document.items['return'].value = escape(location);
-	document.items.submit();
+    document.items['action'].value = 'unread';
+    document.items['return'].value = escape(location);
+    document.items.submit();
 }
 
 function mark_feed_read(id)
 {
-	throb();
-	
-	var url = "view-action.php";
-	var params = "feed=" + id;
-	var complete = function () { refreshlist(); };
-	var options = { method: 'post', parameters: params, onComplete: complete };
-	
-	new Ajax.Request(url, options);
-	
-	return false;
+    throb();
+    
+    var url = "view-action.php";
+    var params = "feed=" + id;
+    var complete = function () { refreshlist(); };
+    var options = { method: 'post', parameters: params, onComplete: complete };
+    
+    new Ajax.Request(url, options);
+    
+    return false;
 }
 
 function add_tag(id, tag)
 {
-	throb();
-	
-	var url = "add-tag.php";
-	var params = "tag=" + tag + "&item=" + id;
-	var complete = function () { refreshlist(); refreshitem(id); };
-	var options = { method: 'get', parameters: params, onComplete: complete };
-	
-	new Ajax.Request(url, options);
-	
-	return false;
+    throb();
+    
+    var url = "add-tag.php";
+    var params = "tag=" + tag + "&item=" + id;
+    var complete = function () { refreshlist(); refreshitem(id); };
+    var options = { method: 'get', parameters: params, onComplete: complete };
+    
+    new Ajax.Request(url, options);
+    
+    return false;
 }
 
 function remove_tag(id, tag)
 {
-	throb();
-	
-	var url = "add-tag.php";
-	var params = "remove=true&tag=" + tag + "&item=" + id;
-	var complete = function () { refreshlist(); refreshitem(id); };
-	var options = { method: 'get', parameters: params, onComplete: complete };
-
-	new Ajax.Request(url, options);
-	
-	return false;
+    throb();
+    
+    var url = "add-tag.php";
+    var params = "remove=true&tag=" + tag + "&item=" + id;
+    var complete = function () { refreshlist(); refreshitem(id); };
+    var options = { method: 'get', parameters: params, onComplete: complete };
+    
+    new Ajax.Request(url, options);
+    
+    return false;
 }
 
 function toggle_favorite(id)
 {
-	throb();
-	
+    throb();
+    
     image = $('fav' + id);    
-
-	var url = "add-tag.php?tag=star";
-	var params = "&item=" + id;
-	var complete = function () { refreshlist(); refreshitem(id); };
-	var options = { method: 'get', parameters: params, onComplete: complete };
-
+    
+    var url = "add-tag.php?tag=star";
+    var params = "&item=" + id;
+    var complete = function () { refreshlist(); refreshitem(id); };
+    var options = { method: 'get', parameters: params, onComplete: complete };
+    
     if(image.star)
     {
-    	params += "&remove=true";
-    	var complete = function() { image.src='image/star-off.gif'; image.star = false; refreshlist(); };
+        params += "&remove=true";
+        var complete = function() { image.src='image/star-off.gif'; image.star = false; refreshlist(); };
     }
     else
     {
-    	var complete = function() { image.src='image/star-on.gif'; image.star = true; refreshlist(); };
+        var complete = function() { image.src='image/star-on.gif'; image.star = true; refreshlist(); };
     }
     
-    var options = { method: 'get', parameters: params, onComplete: complete };	
-	new Ajax.Request(url, options);
-
+    var options = { method: 'get', parameters: params, onComplete: complete };  
+    new Ajax.Request(url, options);
+    
     return false;
 }
 
 function refreshitem(id)
 {
-	throb();
-	
-	var url = 'item.php';
-	var params = 'id=' + id;
-	new Ajax.Updater($("i"+id), url, {method: 'get', parameters: params });
+    throb();
+    
+    var url = 'item.php';
+    var params = 'id=' + id;
+    new Ajax.Updater($("i"+id), url, {method: 'get', parameters: params });
 }
 
 
 function refreshlist()
 {
-	throb();
-
-	var url = 'sidebar.php';
-	var params = "what=" + what + "&when=" + when;
-	new Ajax.Updater($('sidebar'), url, {method: 'get', parameters: params });
+    throb();
+    
+    var url = 'sidebar.php';
+    var params = "what=" + what + "&when=" + when;
+    new Ajax.Updater($('sidebar'), url, {method: 'get', parameters: params });
 }
 
 function throb()
 {
-  Element.show('throbber');
+    Element.show('throbber');
 }
+
