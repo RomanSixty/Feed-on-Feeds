318b0acfde7f2c2327efd83a6f0963b78c1bb921
steveminutillo <steveminutillo@a5d648f4-9e1a-0410-98de-79dcf2039bee>
2007-06-08 01:45:27 +0000

    *ahem*
    git-svn-id: http://feed-on-feeds.googlecode.com/svn/trunk@45 a5d648f4-9e1a-0410-98de-79dcf2039bee

--- fof-config.php
+++ fof-config.php
@@ -5,7 +5,7 @@
  * config.php - modify this file with your database settings
  *
  *
- * Copyright (C) 2004-2006 Stephen Minutillo
+ * Copyright (C) 2004-2007 Stephen Minutillo
  * steve@minutillo.com - http://minutillo.com/steve/
  *
  * Distributed under the GPL - see LICENSE
@@ -13,12 +13,17 @@
  */
 
 
+// Difference, in hours, between your server and your local time zone.
+
+define('FOF_TIME_OFFSET', 0);
+
+
 // Database connection information.  Host, username, password, database name.
 
-define('FOF_DB_HOST', "feedb.feedonfeeds.com");
-define('FOF_DB_USER', "feedb");
+define('FOF_DB_HOST', "host.example.com");
+define('FOF_DB_USER', "username");
 define('FOF_DB_PASS', "password");
-define('FOF_DB_DBNAME', "feedb");
+define('FOF_DB_DBNAME', "database");
 
 
 // The rest you should not need to change
@@ -31,7 +36,7 @@ define('FOF_HOWMANY', 50);
 
 // DB table names
 
-define('FOF_DB_PREFIX', "fof_test_");
+define('FOF_DB_PREFIX', "fof_");
 
 define('FOF_FEED_TABLE', FOF_DB_PREFIX . "feed");
 define('FOF_ITEM_TABLE', FOF_DB_PREFIX . "item");
