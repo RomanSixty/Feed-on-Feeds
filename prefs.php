81bbdbe73e048d9d8db23e06c126c8af16b88657
steveminutillo <steveminutillo@a5d648f4-9e1a-0410-98de-79dcf2039bee>
2007-06-09 00:49:56 +0000

    make uninstall button slightly less ugly
    git-svn-id: http://feed-on-feeds.googlecode.com/svn/trunk@51 a5d648f4-9e1a-0410-98de-79dcf2039bee

--- prefs.php
+++ prefs.php
@@ -94,8 +94,8 @@ No Admin options yet!<br><br>
 <input type=submit name=options value="Save Options">
 </form>
 <br>
-<form method="get" action="uninstall.php" style="border: 1px solid black; margin: 10px; padding: 10px;" onsubmit="return confirm('Really?  This will delete all the database tables!')">
-<center><input type=submit name=uninstall value="Uninstall Feed on Feeds" style="background-color: red"></center>
+<form method="get" action="uninstall.php" onsubmit="return confirm('Really?  This will delete all the database tables!')">
+<center><input type=submit name=uninstall value="Uninstall Feed on Feeds" style="background-color: #ff9999"></center>
 </form>
 
 <?php } ?>
