0afe34121344120bb04d4c0ce78a0086710e7c89
Alexander Schulze <github@lxhome.de>
2013-05-20 18:05:02 +0200

    dynamic updates must be off by default otherwise preferences will always check it by default and next save will enable it again

--- classes/fof-prefs.php
+++ classes/fof-prefs.php
@@ -21,14 +21,14 @@ class FoF_Prefs
     function FoF_Prefs($user_id)
     {
         global $FOF_USER_TABLE;
-	
+
 	$this->user_id = $user_id;
-	
+
         $result = fof_safe_query("select user_prefs from $FOF_USER_TABLE where user_id = %d", $user_id);
         $prefs = unserialize( fof_db_get_row ( $result, 'user_prefs' ));
         if(!is_array($prefs)) $prefs = array();
         $this->prefs = $prefs;
-	
+
         if($user_id != 1)
         {
 	    $result = fof_safe_query("select user_prefs from $FOF_USER_TABLE where user_id = 1");
@@ -40,23 +40,23 @@ class FoF_Prefs
         {
 	    $this->admin_prefs = $prefs;
         }
-	
+
         $this->populate_defaults();
-	
+
         if($user_id == 1)
         {
 	    $this->prefs = array_merge($this->prefs, $this->admin_prefs);
         }
     }
-    
+
     function &instance()
     {
         static $instance;
         if(!isset($instance)) $instance = new FoF_Prefs(fof_current_user());
-	
+
         return $instance;
     }
-    
+
     function populate_defaults()
     {
         $defaults = array(
@@ -67,16 +67,16 @@ class FoF_Prefs
             "sharing" => "no",
             "feed_order" => "feed_title",
             "feed_direction" => "asc",
-            );
+        );
 
         $admin_defaults = array(
             "purge" => 30,
             "autotimeout" => 30,
             "manualtimeout" => 15,
             "logging" => false,
-	    "match_similarity" => 90,
-	    "dynupdates" => true,
-             );
+            "match_similarity" => 90,
+            "dynupdates" => false,
+        );
 
         $this->stuff_array($this->prefs, $defaults);
         $this->stuff_array($this->admin_prefs, $admin_defaults);
