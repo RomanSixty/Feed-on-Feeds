d1094aa174199c9b0f9a1baf57dcc1b16128d7c9
Justin Wind <justin.wind@gmail.com>
2013-08-10 16:19:06 -0700

    Cleanup of enclosure fixes.
    Don't display enclosed images by default.
    Don't overwrite article title with enclosure titles.
    Each enclosure and corresponding display handle now in its own div.

--- plugins/enclosures.php
+++ plugins/enclosures.php
@@ -27,15 +27,17 @@ function fof_enclosures($item, $link, $title, $content) {
 		||  $type === 'image') {
 
 			$description = $enclosure->get_description();
-			$title = htmlentities($enclosure->get_title(), ENT_QUOTES);
-			$enc_html[] = '<a href="#" onclick="show_enclosure(event); return false;" title="' . $title . '">' .
-			                $show_handle .
-			              '</a>' .
-			              '<div class="enclosure">' .
-			              '<a href="' . $enclosure_link . '">' .
-			                '<img src="' . $enclosure_link . '" title="' . $title . '" alt="" />' .
-			              '</a>' .
-			              (empty($description) ? '' : ('<p>' . $description . '</p>')) .
+			$enclosure_title = htmlentities($enclosure->get_title(), ENT_QUOTES);
+			$enc_html[] = '<div>' .
+			                '<a href="#" onclick="show_enclosure(event); return false;" title="' . $enclosure_title . '">' .
+			                  $show_handle .
+			                '</a>' .
+			                '<div class="enclosure" style="display:none;">' .
+			                '<a href="' . $enclosure_link . '">' .
+			                  '<img src="' . $enclosure_link . '" title="' . $enclosure_title . '" alt="" />' .
+			                '</a>' .
+			                (empty($description) ? '' : ('<p>' . $description . '</p>')) .
+			              '</div>' .
 			            '</div>' . "\n";
 			continue;
 		}
@@ -45,7 +47,7 @@ function fof_enclosures($item, $link, $title, $content) {
 			'audio' => 'plugins/place_audio.png',
 			'video' => 'plugins/place_video.png',
 			'mediaplayer' => 'plugins/mediaplayer.swf',
-			'alt' => $enclosure_link(),
+			'alt' => $enclosure_link,
 		);
 
 		$enc_html[] = '<a href="#" onclick="show_enclosure(event); return false;" title="' . htmlentities($enclosure->get_title(), ENT_QUOTES) . '">' .
