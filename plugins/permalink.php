edb406b2ed782b85ad44142ccc88ccd66aa08470
fluffy <fluffy@beesbuzz.biz>
2013-09-17 19:45:15 -0700

    Add permalink to bottom bar, for easier post-item-reading opening

---
+++ plugins/permalink.php
@@ -0,0 +1,12 @@
+<?php 
+
+fof_add_item_widget('fof_permalink');
+
+function fof_permalink($item)
+{
+    $url = html_entity_decode($item['item_link']);
+
+    return '<a href="' . $url . '">permalink</a>';
+}
+?>
+
