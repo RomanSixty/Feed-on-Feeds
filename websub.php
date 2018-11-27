ed0c843beff2bb6205a42e37183fc42da9c5da34
fluffy <fluffy@beesbuzz.biz>
2018-11-27 14:25:31 -0800

    add a little more logging

--- websub.php
+++ websub.php
@@ -54,6 +54,7 @@ if ($_GET['hub_mode'] == 'subscribe') {
 }
 
 // We're responding to a push response.
+fof_log("Got a WebSub push notification for feed $feed_id");
 fof_update_feed($feed_id, file_get_contents('php://input'));
 ?>
 
