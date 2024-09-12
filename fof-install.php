ca6fccc5639da9d13cdc54c0125561a5ce0c80e9
fluffy <fluffy@beesbuzz.biz>
2024-09-12 12:16:12 -0700

    Add composer install check

--- fof-install.php
+++ fof-install.php
@@ -10,6 +10,20 @@ $fof_no_login = true;
 /* installer is running if true, skip auth, no user_id will be set, no prefs loaded, no plugins initialized */
 $fof_installer = true;
 
+// detect missing composer stuff
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+    die('Missing Composer dependencies. Please ensure that you have installed <a href="https://getcomposer.org/">composer</a> and run <tt>composer install</tt>.');
+}
+
+require __DIR__ . '/vendor/autoload.php';
+
+try {
+    urljoin("https://example.com/", "/lemming");
+    new SimplePie();
+} catch (\Throwable $ex) {
+    die("Dependency check failed; did you run <tt>composer install</tt>?<p>$ex");
+}
+
 /* Pull in the fof core, config will be parsed and database will be connected. */
 require_once 'fof-main.php';
 
